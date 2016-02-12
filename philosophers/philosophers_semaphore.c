e6abf8903bbc8384328ff2ebaa1339640faa52b5
Krzysztof Korolczuk <krzysztofkorolczuk2@gmail.com>
2016-02-12 23:11:24 +0100

    Philosophers semaphore example, POSIX threads

--- philosophers/philosophers_semaphore.c
+++ philosophers/philosophers_semaphore.c
@@ -1,11 +1,12 @@
 #include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h> 
 
 pthread_t philosopher[5];
 pthread_mutex_t chopstick[5] = {PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER,
                                 PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER};
-
 struct philosopher_t {
     int nr;
     int left;
@@ -17,9 +18,12 @@ int threadError(char *msg, int nr) {
     exit(1);
 }
 
-void *philosopher_deadlock(void *p) {
+sem_t sem;
+
+void *philosopher_semaphore(void *p) {
     struct philosopher_t philosopher = *((struct philosopher_t *) (p));
     while (1) {
+        sem_wait(&sem); // Semaphore up
         if (!pthread_mutex_lock(&chopstick[philosopher.left])) {
             printf("%d pick up's left chopstick \n", philosopher.nr);
             while (1) {
@@ -31,10 +35,13 @@ void *philosopher_deadlock(void *p) {
                 }
             }
         }
+        sem_post(&sem); //Semaphore down
     }
 }
 
 int main() {
+
+    sem_init(&sem, 0, 4); //Initialization of semaphore counting to 4
 
     struct philosopher_t *p0_t, *p1_t, *p2_t, *p3_t, *p4_t;
     p0_t = malloc(sizeof(struct philosopher_t));
@@ -44,16 +51,16 @@ int main() {
     p4_t = malloc(sizeof(struct philosopher_t));
 
     p0_t->nr = 0;
-    p0_t->left = 1;
-    p0_t->right = 0;
+    p0_t->left = 0;
+    p0_t->right = 1;
 
     p1_t->nr = 1;
     p1_t->left = 1;
     p1_t->right = 2;
 
     p2_t->nr = 2;
-    p2_t->left = 3;
-    p2_t->right = 2;
+    p2_t->left = 2;
+    p2_t->right = 3;
 
     p3_t->nr = 3;
     p3_t->left = 3;
@@ -63,24 +70,24 @@ int main() {
     p4_t->left = 4;
     p4_t->right = 0;
 
-    if (pthread_create(&philosopher[p0_t->nr], NULL, philosopher_deadlock, p0_t)) {
+    if (pthread_create(&philosopher[p0_t->nr], NULL, philosopher_semaphore, p0_t)) {
         threadError("Error while creating philosopher %d\n", p0_t->nr);
     }
-    if (pthread_create(&philosopher[p1_t->nr], NULL, philosopher_deadlock, p1_t)) {
+    if (pthread_create(&philosopher[p1_t->nr], NULL, philosopher_semaphore, p1_t)) {
         threadError("Error while creating philosopher %d\n", p1_t->nr);
     }
-    if (pthread_create(&philosopher[p2_t->nr], NULL, philosopher_deadlock, p2_t)) {
+    if (pthread_create(&philosopher[p2_t->nr], NULL, philosopher_semaphore, p2_t)) {
         threadError("Error while creating philosopher %d\n", p2_t->nr);
     }
-    if (pthread_create(&philosopher[p3_t->nr], NULL, philosopher_deadlock, p3_t)) {
+    if (pthread_create(&philosopher[p3_t->nr], NULL, philosopher_semaphore, p3_t)) {
         threadError("Error while creating philosopher %d\n", p4_t->nr);
     }
-    if (pthread_create(&philosopher[p4_t->nr], NULL, philosopher_deadlock, p4_t)) {
+    if (pthread_create(&philosopher[p4_t->nr], NULL, philosopher_semaphore, p4_t)) {
         threadError("Error while creating philosopher %d\n", p4_t->nr);
     }
 
     if (pthread_join(philosopher[p0_t->nr], NULL)) {
-        threadError("Error while ending asymmetric philosopher %d\n", p0_t->nr);
+        threadError("Error while ending philosopher %d\n", p0_t->nr);
     }
     if (pthread_join(philosopher[p1_t->nr], NULL)) {
         threadError("Error while ending philosopher %d\n", p1_t->nr);
