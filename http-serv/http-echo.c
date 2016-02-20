bac20c0403c2ce35de54168189e03f455144e4b8
Krzysztof Korolczuk <krzysztofkorolczuk2@gmail.com>
2016-02-20 19:19:26 +0100

    http echo create

---
+++ http-serv/http-echo.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define BUFFER_SIZE 8096
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+
+int main(int argc, char **argv) {
+    int pid, listen_fd, socket_fd;
+    socklen_t length;
+    static struct sockaddr_in cli_addr;
+    static struct sockaddr_in serv_addr;
+
+    if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("system call socket");
+        EXIT_FAILURE;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(80);
+
+    if (bind(listen_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+        perror("system call bind");
+        EXIT_FAILURE;
+    }
+    if (listen(listen_fd, 64) < 0) {
+        perror("system call listen");
+        EXIT_FAILURE;
+    }
+
+
+    while (1) {
+        length = sizeof(cli_addr);
+        if ((socket_fd = accept(listen_fd, (struct sockaddr *) &cli_addr, &length)) < 0)
+            printf("system call accept");
+        if ((pid = fork()) < 0) {
+            printf("system call fork");
+        }
+        else {
+            if (pid == 0) {    /* child */
+                close(listen_fd);
+                static char buffer[BUFFER_SIZE + 1]; /* static so zero filled */
+                read(socket_fd, buffer, BUFFER_SIZE);    /* read Web request in one go */
+                write(socket_fd, buffer, strlen(buffer));
+                sleep(1);
+                close(socket_fd);
+                exit(1);
+            } else {    /* parent */
+                close(socket_fd);
+            }
+        }
+    }
+    EXIT_SUCCESS;
+}
+
+#pragma clang diagnostic pop
